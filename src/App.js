a0923f6bde7cc69485a6587fe07b7cb35fc41de4
Andres <chimpinano@yahoo.com.mx>
2019-01-12 09:54:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { NotFound } from './pages/NotFound'
+
 import './App.css';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/detail/:movieId' component={Detail} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
